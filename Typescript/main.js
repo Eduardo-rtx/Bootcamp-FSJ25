320a82bc707475414c5f26aff13dc6be636c8867
Eduartds <luiserm97@gmail.com>
2024-09-09 19:35:27 -0600

    more files

---
+++ Typescript/main.js
@@ -0,0 +1,34 @@
+//Declaracion de variables en JS
+var variable = "";
+//Declaracion de variables + tipo de dato en TS
+var cadenaTexto = "Luis";
+var numero = 123;
+var booleano = true;
+//Declaracion de arreglos o arrays
+var arreglo = [1, 2, 3, 4, 5]; //Ambas formas son equivalentes y eligen una u otra es una cuestión de preferencia o estilo de codificación. Forma 2: let arreglo:Array<number> = [1,2,3,4,5];
+var arreglo2 = [1, 2, 3, 4, 5]; //Ambas formas son equivalentes y eligen una u otra es una cuestión de preferencia o estilo de codificación. Forma 1: let arreglo:number[] = [1,2,3,4,5];
+var arregloCualquierTipoDato = [1, 2, 3, 4, 5, "Luis", true]; //Es mejor evitar el uso de any, ya que se pierde la ventaja de tener un lenguaje fuertemente tipado.
+var arregloStrings = ["Luis", "Pedro", "Juan"];
+//Declaracion de tuplas
+//Es un array con indices limitados y con su tipo de dato declarado previamente
+//Una tupla es un arreglo de elementos que tienen un tipo de dato fijo y una cantidad fija de elementos. 
+var datosUsuario;
+datosUsuario = [1, "Luis"];
+//array numerico o string
+var arrayNumOrString = ["13", "solo texto o solo nums"]; //Solo puede ser un array de numeros o de strings
+var arrayNumString = ["Hola", 1234, "ok"]; //Puede ser un array de numeros y de strings
+//Funciones en TS
+function tipoVoid() {
+    console.log("Hola Mundo");
+}
+function sumar() {
+    return 2 + 2;
+}
+function sumarOConcatenar() {
+    return 3 + 3;
+}
+function saludar(nombre) {
+    //template string -> plantillas literales -> ingresar un datos de js en un string
+    //"Hola como estas? "+nombre;
+    return "Hola como estas? ".concat(nombre);
+}
---
+++ Typescript/main.ts
@@ -0,0 +1,44 @@
+//Declaracion de variables en JS
+let uniqueVariable = "";
+
+//Declaracion de variables + tipo de dato en TS
+let cadenaTexto:string = "Luis";
+let numero:number = 123;
+let booleano:boolean = true;
+
+//Declaracion de arreglos o arrays
+let arreglo:number[] = [1,2,3,4,5];//Ambas formas son equivalentes y eligen una u otra es una cuestión de preferencia o estilo de codificación. Forma 2: let arreglo:Array<number> = [1,2,3,4,5];
+let arreglo2:Array<number> = [1,2,3,4,5];//Ambas formas son equivalentes y eligen una u otra es una cuestión de preferencia o estilo de codificación. Forma 1: let arreglo:number[] = [1,2,3,4,5];
+let arregloCualquierTipoDato:any[] = [1,2,3,4,5,"Luis",true];//Es mejor evitar el uso de any, ya que se pierde la ventaja de tener un lenguaje fuertemente tipado.
+let arregloStrings:string[] = ["Luis","Pedro","Juan"];
+
+//Declaracion de tuplas
+//Es un array con indices limitados y con su tipo de dato declarado previamente
+//Una tupla es un arreglo de elementos que tienen un tipo de dato fijo y una cantidad fija de elementos. 
+let datosUsuario:[number,string];
+datosUsuario = [1,"Luis"];
+
+//array numerico o string
+let arrayNumOrString:number[]|string[] = ["13", "solo texto o solo nums"];//Solo puede ser un array de numeros o de strings
+let arrayNumString:(number|string)[] = ["Hola", 1234, "ok"];//Puede ser un array de numeros y de strings
+
+//Funciones en TS
+function tipoVoid():void{
+    console.log("Hola Mundo");
+}
+
+function sumar():number{
+    return 2+2;
+}
+
+function sumarOConcatenar():number|string{
+    return 3+3;
+}
+
+function saludar(nombre:string):string{
+    //template string -> plantillas literales -> ingresar un datos de js en un string
+    //"Hola como estas? "+nombre;
+    return `Hola como estas? ${nombre}`;
+}
+
+
